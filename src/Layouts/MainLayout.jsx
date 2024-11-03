796633a6a8d42500ad820997c7d84fba9d4ae9d6
appoclayptor <shahreza1414@gmail.com>
2024-11-04 04:07:16 +0600

    added navbar

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Header></Header>
+            {/* <Footer></Footer> */}
+        </div>
+    );
+};
+
+export default MainLayout;
