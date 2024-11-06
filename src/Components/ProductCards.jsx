7833186c57acf8858f6eb75db68302413ee7ab04
appoclayptor <shahreza1414@gmail.com>
2024-11-06 07:19:03 +0600

    made a functional toggle view more button

--- src/Components/ProductCards.jsx
+++ src/Components/ProductCards.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData, useLocation, useParams } from 'react-router-dom';
 import SingleCard from './SingleCard';
 
 const ProductCards = () => {
+    const location = useLocation()
+    const isHome = location.pathname === '/';
     const productsData = useLoaderData();
     const {category} = useParams();
     const [products, setProducts] = useState([]);
+    const [showAll, setShowAll] = useState(false);
     useEffect(() => {
-        if(!category || category === 'all'){
-            setProducts(productsData.slice(0,6))
-        }
-        else{
+        if(category){
             const filteredByCategory = [...productsData].filter(product => product.category === category)
             setProducts(filteredByCategory)
         }
-    }, [category, productsData]);
+        else if (showAll){
+            setProducts(productsData)
+        }
+        else{
+            setProducts(productsData.slice(0,6))
+        }
+    }, [category, productsData, showAll]);
+
+    const viewAllBtnHandler = () => {
+        setShowAll(!showAll)
+    }
+
 
     return (
             <div>
@@ -23,7 +34,9 @@ const ProductCards = () => {
                         products.map(product => <SingleCard key={product.product_title} product={product}></SingleCard>)
                     }
                 </div>
-                <button className='btn mt-5 rounded-xl bg-primary text-white hover:bg-primary'>View All</button>
+                {
+                isHome && <button onClick={viewAllBtnHandler} className='btn mt-5 rounded-xl bg-primary text-white hover:bg-primary'>{showAll? 'Show Less' : 'View All'}</button>
+                }
             </div>
         
     );
