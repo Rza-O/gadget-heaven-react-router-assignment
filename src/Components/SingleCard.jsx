e10fd31e670294feccdb3e4b5f93015b728fe405
appoclayptor <shahreza1414@gmail.com>
2024-11-06 06:20:24 +0600

    randomized fake data and also created the cards

--- src/Components/SingleCard.jsx
+++ src/Components/SingleCard.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 const SingleCard = ({product}) => {
     const { product_image, product_title, price } = product;
     return (
-        <div>
-            <h1>{product_title}</h1>
+        <div className='border p-5 rounded-2xl'>
+            <img className='object-fit w-72 h-44 rounded-2xl mb-6' src={product_image} alt="" />
+            <h3 className='mb-3'>{product_title}</h3>
+            <p className='mb-4'>{price}</p>
+            <button>View Details</button>
         </div>
     );
 };
