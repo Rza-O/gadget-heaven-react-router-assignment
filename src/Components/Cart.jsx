7ffe897605c8af4c40e18b3e623c820b722005ae
appoclayptor <shahreza1414@gmail.com>
2024-11-07 14:45:08 +0600

    Made the sort by price functionality

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useCartWishList from '../Utility';
 import CartCard from './CartCard';
-import { FaSortAmountDown } from "react-icons/fa";
+import { FaSortAmountDown, FaSortAmountUp } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { ImSad } from "react-icons/im";
 
@@ -9,21 +9,33 @@ const Cart = () => {
     const { cart, removeCartItem } = useCartWishList()
     const totalCost = cart.reduce((sum, item) => sum + item.price, 0);
     const navigate = useNavigate();
-    // console.log(cart);
+
+    const [isSortedDescending, setIsSortedDescending] = useState(false);
+
+    const sortToggle = () => {
+        setIsSortedDescending(!isSortedDescending)
+    };
+
+    const sortedCart = [...cart].sort(((a, b) => {
+        return isSortedDescending? b.price - a.price : a.price - b.price
+    }))
+
     return (
         <div >
             <div className='w-10/12 mx-auto justify-between flex items-center mt-4'>
                 <h2 className='text-xl font-bold'>Cart</h2>
                 <div className='flex items-center space-x-6'>
                     <h1 className='mr-14 text-xl font-bold'>Total Cost: ${totalCost.toFixed(2)}</h1>
-                    <div className='flex items-center border border-primary text-primary p-3 rounded-3xl gap-1 cursor-pointer hover:bg-gray-200'>Sort by price <FaSortAmountDown /></div>
+                    <div onClick={sortToggle} className='flex items-center border border-primary text-primary p-3 rounded-3xl gap-1 cursor-pointer hover:bg-gray-200'>Sort by price {isSortedDescending?
+                        <FaSortAmountDown /> : <FaSortAmountUp />
+                        }</div>
                     <div className='border px-8 rounded-3xl bg-primary text-white btn'>Purchase</div>
                 </div>
             </div>
             <div className='mt-6'>
                 {
-                    cart.length > 0 ? (cart.map(item => <CartCard removeCartItem={removeCartItem} item={item}></CartCard>))
-                        : (<div className='w-10/12 p-24 mx-auto border text-center space-y-4'>
+                    sortedCart.length > 0 ? (sortedCart.map(item => <CartCard removeCartItem={removeCartItem} item={item}></CartCard>))
+                        : (<div className='w-10/12 p-24 mx-auto text-center space-y-4'>
                             <h1 className='text-4xl'>Your cart is soo... <span className='font-bold flex items-center justify-center mt-4'>Empty!<ImSad className='ml-1' /></span></h1>
                             <p className='text-lg text-black/40'>Choose the right gadget for you today...</p>
                             <button onClick={()=> navigate('/')} className='btn bg-primary text-white rounded-2xl'>Shop Now</button>
